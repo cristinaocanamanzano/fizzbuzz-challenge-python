ed8f2d2882a09172bc5817250c5b2ebd30028370
cristinaocanamanzano <cristinaocana1@gmail.com>
2018-12-11 16:41:42 +0000

    Refactor print_fizzbuzz method by delegating to other new created functions

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,21 @@
+def fizzbuzz():
+    print('FizzBuzz')
+
+def fizz():
+    print('Fizz')
+
+def buzz():
+    print('Buzz')
+
 def print_fizzbuzz():
     for num in range(1, 31):
         if num % 15 is 0:
-            print('FizzBuzz')
+            fizzbuzz()
         elif num % 3 is 0:
-            print('Fizz')
+            fizz()
         elif num % 5 is 0:
-            print('Buzz')
+            buzz()
         else:
             print(num)
+
+print_fizzbuzz()
--- fizzbuzz_spec.py
+++ fizzbuzz_spec.py
@@ -4,7 +4,19 @@ from mock import patch, MagicMock
 from StringIO import StringIO
 
 class FizzBuzzTest(unittest.TestCase):
-    
+    def test_fizzbuzz(self):
+        with patch('sys.stdout', new=StringIO()) as fakeOutput:
+            fizzbuzz()
+            self.assertEqual(fakeOutput.getvalue().strip(), 'FizzBuzz')
+
+    def test_fizz(self):
+        with patch('sys.stdout', new=StringIO()) as fakeOutput:
+            fizz()
+            self.assertEqual(fakeOutput.getvalue().strip(), 'Fizz')
+    def test_buzz(self):
+        with patch('sys.stdout', new=StringIO()) as fakeOutput:
+            buzz()
+            self.assertEqual(fakeOutput.getvalue().strip(), 'Buzz')
     def test_printfizzbuzz(self):
         with patch('sys.stdout', new=StringIO()) as fakeOutput:
             print_fizzbuzz()
