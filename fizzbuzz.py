fe74e55c2752b9af07f1d557040d41033b971dba
cristinaocanamanzano <cristinaocana1@gmail.com>
2018-12-11 16:38:28 +0000

    Print Fizz when number is multiple of 3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,5 +2,7 @@ def print_fizzbuzz():
     for num in range(1, 101):
         if num % 15 is 0:
             print('FizzBuzz')
+        elif num % 3 is 0:
+            print('Fizz')
         else:
             print(num)
