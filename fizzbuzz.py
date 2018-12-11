5643013ad2bc8c08c91c16e4516c968b2121a332
cristinaocanamanzano <cristinaocana1@gmail.com>
2018-12-11 16:39:25 +0000

    Print Buzz when number is multiple of 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,10 @@
 def print_fizzbuzz():
-    for num in range(1, 101):
+    for num in range(1, 31):
         if num % 15 is 0:
             print('FizzBuzz')
         elif num % 3 is 0:
             print('Fizz')
+        elif num % 5 is 0:
+            print('Buzz')
         else:
             print(num)
